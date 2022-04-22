273c30a1217bdf2213474a11b40bbe87b3cdfb8e
Thomas Wilson <P17213482@my365.dmu.ac.uk>
2022-04-22 14:27:40 +0100

    fix

--- Code/Client/Video.py
+++ Code/Client/Video.py
@@ -40,6 +40,20 @@ class VideoStreaming:
             except:  
                 bValid = False
         return bValid
+
+    def face_detect(self,img):
+        if sys.platform.startswith('win') or sys.platform.startswith('darwin'):
+            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            faces = self.face_cascade.detectMultiScale(gray,1.3,5)
+            if len(faces)>0 :
+                for (x,y,w,h) in faces:
+                    self.face_x=float(x+w/2.0)
+                    self.face_y=float(y+h/2.0)
+                    img= cv2.circle(img, (int(self.face_x),int(self.face_y)), int((w+h)/4), (0, 255, 0), 2)
+            else:
+                self.face_x=0
+                self.face_y=0
+        cv2.imwrite('video.jpg',img)
         
     def streaming(self,ip):
         stream_bytes = b' '
@@ -57,6 +71,7 @@ class VideoStreaming:
                 if self.IsValidImage4Bytes(jpg):
                             image = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
                             if self.video_Flag:
+                                self.face_detect(image)
                                 self.video_Flag=False
             except Exception as e:
                 print (e)
